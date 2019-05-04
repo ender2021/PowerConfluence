726ea84d89783bc22feba4251e9b570db0978099
ender2021 <meadmiracle@gmail.com>
2019-05-04 15:14:35 -0700

    Converted Invoke-ConfluenceAddLabelsToContent to cmdlet

--- PowerConfluence/public/api-v1/content/Invoke-ConfluenceAddLabelsToContent.ps1
+++ PowerConfluence/public/api-v1/content/Invoke-ConfluenceAddLabelsToContent.ps1
@@ -1,6 +1,44 @@
 #https://developer.atlassian.com/cloud/confluence/rest/#api-content-id-label-post
-function Invoke-ConfluenceAddLabelsToContent($ConfluenceConnection,$PageID,$LabelName) {
-    $body = @(@{name=$LabelName;prefix="global"})
-    $function = "content/$PageID/label"
-    Send-ConfluenceRestRequest -ConfluenceConnection $ConfluenceConnection -FunctionAddress $function -HttpMethod Post -Body $body
+function Invoke-ConfluenceAddLabelsToContent {
+    [CmdletBinding()]
+    param (
+        # The ID of the page
+        [Parameter(Mandatory,Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Alias("PageId")]
+        [int32]
+        $Id,
+
+        # The name of the label to add
+        [Parameter(Mandatory,Position=1,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [string[]]
+        $Label,
+
+        # The scope of the label
+        [Parameter(Position=2,ValueFromPipelineByPropertyName)]
+        [ValidateSet("global","my","team")]
+        [string]
+        $Scope="global",
+
+        # The ConfluenceConnection object to use for the request
+        [Parameter()]
+        [hashtable]
+        $ConfluenceConnection
+    )
+    begin {
+        $results = @()
+    }
+    process {
+        $functionPath = "/wiki/rest/api/content/$Id/label"
+        $verb = "POST"
+
+        $body = @()
+        $Label | ForEach-Object { $body += @{name = $_; prefix = $Scope} }
+            
+        $literalBody = ConvertTo-Json $body -Compress
+
+        $results += Invoke-ConfluenceRestMethod $ConfluenceConnection $functionPath $verb -LiteralBody $literalBody
+    }
+    end {
+        $results
+    }
 }
