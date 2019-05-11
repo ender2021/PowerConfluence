eaedd9bec679ffbe018546aa22700dc349d50dc2
ender2021 <meadmiracle@gmail.com>
2019-05-11 13:23:26 -0700

    Added Invoke-ConfluenceGetLongRunningTasks

---
+++ PowerConfluence/public/api-v1/long-task/Invoke-ConfluenceGetLongRunningTasks.ps1
@@ -0,0 +1,41 @@
+#https://developer.atlassian.com/cloud/confluence/rest/#api-longtask-get
+function Invoke-ConfluenceGetLongRunningTasks {
+    [CmdletBinding()]
+    param (
+        # The index of the first item to return in the page of results (page offset). The base index is 0.
+        [Parameter(Position=0,ValueFromPipelineByPropertyName)]
+        [int32]
+        $StartAt=0,
+
+        # The maximum number of items to return per page. The default is 100 and the maximum is 100.
+        [Parameter(Position=1,ValueFromPipelineByPropertyName)]
+        [ValidateRange(1,100)]
+        [int32]
+        $MaxResults=100,
+
+
+        # The ConfluenceConnection object to use for the request
+        [Parameter(Position=2)]
+        [hashtable]
+        $ConfluenceConnection
+    )
+    begin {
+        $results = @()
+    }
+    process {
+        $RestArgs = @{
+            ConfluenceConnection = $ConfluenceConnection
+            FunctionPath = "/wiki/rest/api/longtask"
+            HttpMethod = "GET"
+            Query = @{
+                start = $StartAt
+                limit = $MaxResults
+            }
+        }
+
+        $results += Invoke-ConfluenceRestMethod @RestArgs
+    }
+    end {
+        $results
+    }
+}
---
+++ demos/Demo-LongTask.ps1
@@ -0,0 +1,18 @@
+#import PowerConfluence
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \..\PowerConfluence\PowerConfluence.psm1) -Force
+
+#import the variable $ConfluenceCredentials
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath \credentials\Credentials.psm1) -Force
+
+#open a new Confluence session
+Open-ConfluenceSession -UserName $ConfluenceCredentials.UserName -Password $ConfluenceCredentials.ApiToken -HostName $ConfluenceCredentials.HostName
+
+#do tests here
+
+#GET LONG RUNNING TASKS
+(Invoke-ConfluenceGetLongRunningTasks).results
+
+#end tests
+
+#close the Confluence session
+Close-ConfluenceSession
